31e991f4bfb1eb8e411f3ce8b40dfd67d0e963fd
LauchlynDowns <49620443+LauchlynDowns@users.noreply.github.com>
2020-05-16 13:08:20 +0100

    added more javascript

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,17 @@
 
+//script to make the mobile menu work
+//variables to store ui elements
+const menubutton = document.getElementById("dropdownbutton");
+const mobileicon = document.getElementById("mobileicon");
+const navbarbuttons = document.getElementById("navbar-linkarea-mobile");
+const navbar = document.getElementById("navbar");
+
+
+//event listener to check if the button has been pressed
+menubutton.addEventListener("click", function() {
+    //clear the navbar
+    mobileicon.style.display = "none";
+    menubutton.style.display = "none";
+    navbarbuttons.style.height = "80vh";
+    navbar.style.height = "80vh";
+})
