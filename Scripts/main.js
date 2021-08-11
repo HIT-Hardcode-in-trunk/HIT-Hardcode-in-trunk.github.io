9406a26228fe617f3773895574ed21458dc8ad29
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-11 16:40:33 +0100

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,36 +3,36 @@ var resultList = [];
 
 
 
-function addSourceToVideo(element, src, type) {
-    var source = document.createElement('source');
+// function addSourceToVideo(element, src, type) {
+//     var source = document.createElement('source');
 
-    source.src = src;
-    source.type = type;
+//     source.src = src;
+//     source.type = type;
 
-    element.appendChild(source);
-}
+//     element.appendChild(source);
+// }
 
-var video1 = document.createElement('video');
-var video2 = document.createElement('video');
-var video3 = document.createElement('video');
-var att1 = document.getElementById("new");
-var att2 = document.getElementById("new");
-var att3 = document.getElementById("new");
-
-
-att1.appendChild(video1);
-att2.appendChild(video2);
-att3.appendChild(video3);
+// var video1 = document.createElement('video');
+// var video2 = document.createElement('video');
+// var video3 = document.createElement('video');
+// var att1 = document.getElementById("new");
+// var att2 = document.getElementById("new");
+// var att3 = document.getElementById("new");
 
 
-addSourceToVideo(video1, 'video/good_answer.mp4', 'video/mp4');
-addSourceToVideo(video2, 'video/good_answer.mp4', 'video/mp4');
-addSourceToVideo(video3, 'video/good_answer.mp4', 'video/mp4');
+// att1.appendChild(video1);
+// att2.appendChild(video2);
+// att3.appendChild(video3);
 
 
-video1.play();
-video2.play();
-video3.play();
+// addSourceToVideo(video1, 'video/good_answer.mp4', 'video/mp4');
+// addSourceToVideo(video2, 'video/good_answer.mp4', 'video/mp4');
+// addSourceToVideo(video3, 'video/good_answer.mp4', 'video/mp4');
+
+
+// video1.play();
+// video2.play();
+// video3.play();
 
 
 
