a5dda0126f084f494bcf1d48ee963516896bffec
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-13 15:49:32 +0100

    Update main.js

--- game2/Scripts/main.js
+++ game2/Scripts/main.js
@@ -19,6 +19,7 @@ const quizdata = [
         
         videoOption: "videoOption_1",
 playerId: "kaltura_player_1628782914",
+playerId: "kaltura_player_1628693936",
         options: ["Option 1", "Option 2", "Option 3"],
         answer: ["Option 2"],
         category: 2,
