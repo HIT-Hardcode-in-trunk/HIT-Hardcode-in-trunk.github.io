b8ef1e12bc1e7b72490c70602a5256185a4488f4
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-16 16:23:40 +0100

    Update main.js

--- game2/Scripts/main.js
+++ game2/Scripts/main.js
@@ -6,8 +6,8 @@ var video1 = document.getElementsByClassName ('.video1');
 const quizdata = [
     {
         question: "Why have you applied for the job?",
-        videoOption: ["videoOption_2"],
-        playerId: ["kaltura_player_1628782791","kaltura_player_1628782968"],
+        videoOption: "videoOption_2",
+        playerId: "kaltura_player_1628782968",
         options: ["Answer A", "Answer B:", "Answer C:"],
         answer: ["Answer B:"],
         category: 1,
