911186b6aa22ea398883ba8e780004f6a69b7b39
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-18 10:33:56 +0100

    Delete lottie.js

--- game2/Scripts/lottie.js
+++
@@ -1,8 +0,0 @@
-const svgContainer = document.getElementById('svg');
-const animItem = bodymovin.loadAnimation({
-    wrapper: svgContainer,
-    animType: 'svg',
-    loop: true,
-    autoplay: true,
-    path: "https://assets4.lottiefiles.com/packages/lf20_wycox7py.json"
-});
