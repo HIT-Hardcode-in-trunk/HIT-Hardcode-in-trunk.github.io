923f8d78b08e6eec0c58cc52ca0444dad76868ab
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-23 12:07:22 +0100

    Update main.js

--- game_competency/Scripts/main.js
+++ game_competency/Scripts/main.js
@@ -17,7 +17,13 @@ const quizdata = [
         video: "videoOption_0",
         options: ["Answer A", "Answer B", "Answer C"],
         answer: "Answer A",
-        answerDescription: ["Answer A: This is the best answer as it follows the STARR method and demonstrates well the candidate’s ability to work in a team. It also covers many of the skills that the employer is looking for."],
+        answerDescription: [
+            
+            "Answer A: This is the best answer as it follows the STARR method and demonstrates well the candidate’s ability to work in a team. It also covers many of the skills that the employer is looking for.", 
+            "Answer B: This is the poorest answer as it isn’t full enough – it doesn’t give a detailed description and doesn’t follow the STARR method.", 
+            "Answer C: This is a fairly good answer as it follows the STARR method, but the candidate should have concentrated more on describing specifically what they did, rather than the team as a whole."
+        
+        ],
         category: 1,
         optionScores :[0, 50, 10]
     },
