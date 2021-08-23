70b44732dc7eb326d07bc9e9465d17d5c52c9b98
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-23 17:29:09 +0100

    Update app2.js

--- Scripts/app2.js
+++ Scripts/app2.js
@@ -10,7 +10,7 @@ function navToggle(e) {
         gsap.to(".line2", 0.5, { rotate: "-45", y: -4, background: "#5A4980" });
         gsap.to(".burger-circle", 1, { boxShadow: "0px 0px 20px 0px violet" });
         gsap.to(".right-chevron", 1, { boxShadow: "0px 0px 20px 0px violet" });
-        gsap.to(".nav-bar", 1.5, { clipPath: "circle(2500px at 100% -10%)" });
+        gsap.to(".nav-bar-mobile", 1.5, { clipPath: "circle(2500px at 100% -10%)" });
         document.body.classList.add("hide");
     } else {
         e.target.classList.remove("active");
@@ -19,7 +19,7 @@ function navToggle(e) {
         gsap.to(".burger-circle", 1, { boxShadow: "0px 0px 0px 0px black" });
         gsap.to(".right-chevron", 1, { boxShadow: "0px 0px 0px 0px black" });
         gsap.to(".right-chevron", 1, { boxShadow: "0px 0px 0px 0px black" });
-        gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+        gsap.to(".nav-bar-mobile", 1, { clipPath: "circle(50px at 100% -10%)" });
         document.body.classList.remove("hide");
     }
 }
