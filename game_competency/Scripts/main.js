b37fc91b856769c9bf1e15a1ed79ae908c6296ca
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-08-23 13:24:35 +0100

    Update main.js

--- game_competency/Scripts/main.js
+++ game_competency/Scripts/main.js
@@ -16,6 +16,7 @@ const quizdata = [
         question:"Can you tell me about a time when you successfully completed a task as part of a team?",
         video: "videoOption_0",
         options: ["Answer A", "Answer B", "Answer C"],
+        answer: "Answer A",
         answerDescription: [
             
             "Answer A: This is the best answer as it follows the STARR method and demonstrates well the candidate’s ability to work in a team. It also covers many of the skills that the employer is looking for." + "<br></br>",
@@ -24,7 +25,7 @@ const quizdata = [
         
         ],
 
-        answer: "Answer A",
+        
         category: 1,
         optionScores :[0, 50, 10]
     }
